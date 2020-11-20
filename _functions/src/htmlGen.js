3cf0c134d0f30667c1aae2b972f703396b35b6fc
Fizal Sarif <fizal.sarif@gmail.com>
2020-11-20 00:48:54 -0500

    html template for email, footer, and story page

--- _functions/src/htmlGen.js
+++ _functions/src/htmlGen.js
@@ -1,3 +1,5 @@
+const htmlTemplate = require("./html_template");
+
 function commas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -15,69 +17,37 @@ module.exports = (event) => {
   } = JSON.parse(event.body);
 
   if (type === "contact") {
-    return `
-    <!DOCTYPE html>
-    <html>
-      <style>
-        body {
-          font-family: sans-serif;
-          margin: 25px 50px;
-        }
-        h2 {
-          text-transform: capitalize;
-        }
-      </style>
-      <head></head>
-      <body>
-        <h4>${customer} (${email}) said:</h4>
-        <p>${body}</p>
-        <br>
-        <p>This message was sent from a unmonitored mailbox.</p>
-        <p>Please do not reply to this email. Contact supriya@saffroncateringgy.com with any concerns</p>
-      </body>
-    </html>
-  `;
+    return htmlTemplate(`
+      <h4>${customer} (${email}) said:</h4>
+      <p>${body}</p>
+      <br>
+
+  `);
   }
 
   if (type === "order") {
     const items = Object.values(bag);
     let runningTotal = 0;
-    return `
-      <!DOCTYPE html>
-      <html>
-        <style>
-          body {
-            font-family: sans-serif;
-            margin: 25px 50px;
-          }
-          h2 {
-            text-transform: capitalize;
-          }
-        </style>
-        <head></head>
-        <body>
-          <hr/>
-          <h4>${customer} (${email}, ${phone})</h4>
-          <p>${address}</p>
-          <hr/>
-          <h3>Order Summary</h3>
-          ${items.reduce((p,c) => {
-            runningTotal += c.qty * c.price
-            return p + `
-              <p><b>${c.qty}</b> x ${c.name} <i>${c.option}</i> <b>$${commas(c.qty * c.price)}</b><p>
-              <p>${c.instructions ? `Special instructions: ${c.instructions}` : ""}</p>
-            `
-          }, "")}
-          <br>
-          <p><b>Total:</b> $${commas(runningTotal)}</p>
-          <p><b>Pickup/Delivery Date:</b> ${date}</p>
-          <hr/>
-          <p>A representative will call you soon to confirm your order.</p>
-          <p>This message was sent from a unmonitored mailbox.</p>
-          <p>Please do not reply to this email. Contact supriya@saffroncateringgy.com with any concerns</p>
-        </body>
-      </html>
-    `;
+    return htmlTemplate(`
+      <h4>${customer} (${email}, ${phone})</h4>
+      <p>${address}</p>
+      <hr/>
+      <h3>Items:</h3>
+      ${items.reduce((p,c) => {
+        runningTotal += c.qty * c.price
+        return p + `
+          <p><b>${c.qty}</b> x ${c.name} <i>${c.option}</i> <b>$${commas(c.qty * c.price)}</b><p>
+          <p>${c.instructions ? `Special instructions: ${c.instructions}` : ""}</p>
+        `
+      }, "")}
+      <br>
+      <p><b>Total:</b> $${commas(runningTotal)}</p>
+      <p><b>Pickup/Delivery Date:</b> ${date}</p>
+      <hr/>
+      <p>A representative will call you soon to confirm your order.</p>
+      <p>This message was sent from a unmonitored mailbox.</p>
+      <p>Please do not reply to this email. Contact supriya@saffroncateringgy.com with any concerns</p>
+    `, true);
   }
 
 
