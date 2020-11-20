c5072908d53240c9e17a1828ae73b5369f7aaa7e
Fizal Sarif <fizal.sarif@gmail.com>
2020-11-19 20:49:00 -0500

    working on html template

---
+++ _functions/src/html_template.js
@@ -0,0 +1,387 @@
+module.exports = body => `
+<br><br><u><=
+/u>
+   =20
+     =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+       =20
+     =20
+       =20
+
+       =20
+     =20
+
+   =20
+       =20
+   =20
+   =20
+ =20
+       =20
+       =20
+       =20
+     =20
+      <div style=3D"background-color:#ffffff">
+       =20
+       =20
+      <div style=3D"background-color:#ffffff">
+       =20
+     =20
+     =20
+   =20
+     =20
+      <div style=3D"Margin:0px auto;max-width:600px">
+       =20
+        <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=
+=3D"0" style=3D"width:100%">
+          <tbody>
+            <tr>
+              <td style=3D"direction:ltr;font-size:0px;padding:9px 0px 9px =
+0px;text-align:center;vertical-align:top">
+               =20
+           =20
+      <div class=3D"m_902445541223155851mj-column-per-100" style=3D"font-si=
+ze:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:t=
+op;width:100%">
+       =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"vert=
+ical-align:top" width=3D"100%">
+       =20
+            <tbody><tr>
+              <td align=3D"center" style=3D"font-size:0px;padding:0px 0px 0=
+px 0px;word-break:break-word">
+               =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"bord=
+er-collapse:collapse;border-spacing:0px">
+        <tbody>
+          <tr>
+            <td style=3D"width:600px">
+             =20
+      <img height=3D"auto" src=3D"https://s3-eu-west-1.amazonaws.com/topoli=
+o/uploads/5faadab2e31cd/1605067221.jpg" style=3D"border:0;display:block;out=
+line:none;text-decoration:none;height:auto;width:100%;font-size:13px" width=
+=3D"600">
+   =20
+            </td>
+          </tr>
+        </tbody>
+      </table>
+   =20
+              </td>
+            </tr>
+         =20
+      </tbody></table>
+   =20
+      </div>
+   =20
+         =20
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       =20
+      </div>
+   =20
+     =20
+     =20
+   =20
+     =20
+      <div style=3D"Margin:0px auto;max-width:600px">
+       =20
+        <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=
+=3D"0" style=3D"width:100%">
+          <tbody>
+            <tr>
+              <td style=3D"direction:ltr;font-size:0px;padding:9px 0px 9px =
+0px;text-align:center;vertical-align:top">
+               =20
+           =20
+      <div class=3D"m_902445541223155851mj-column-per-100" style=3D"font-si=
+ze:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:t=
+op;width:100%">
+       =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"vert=
+ical-align:top" width=3D"100%">
+       =20
+            <tbody><tr>
+              <td align=3D"center" style=3D"font-size:0px;padding:0px 0px 0=
+px 0px;word-break:break-word">
+               =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"bord=
+er-collapse:collapse;border-spacing:0px">
+        <tbody>
+          <tr>
+            <td style=3D"width:600px">
+             =20
+      <img height=3D"auto" src=3D"https://s3-eu-west-1.amazonaws.com/topoli=
+o/uploads/5faadab2e31cd/1605067652.jpg" style=3D"border:0;display:block;out=
+line:none;text-decoration:none;height:auto;width:100%;font-size:13px" width=
+=3D"600">
+   =20
+            </td>
+          </tr>
+        </tbody>
+      </table>
+   =20
+              </td>
+            </tr>
+         =20
+            <tr>
+              <td style=3D"font-size:0px;padding:10px 25px;padding-top:10px=
+;padding-right:10px;padding-bottom:10px;padding-left:25px;word-break:break-=
+word">
+               =20
+      <p style=3D"border-top:solid 3px #000000;font-size:1;margin:0px auto;=
+width:100%">
+      </p>
+     =20
+     =20
+   =20
+   =20
+              </td>
+            </tr>
+         =20
+      </tbody></table>
+   =20
+      </div>
+   =20
+         =20
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       =20
+      </div>
+   =20
+     =20
+     =20
+   =20
+     =20
+      <div style=3D"Margin:0px auto;max-width:600px">
+       =20
+        <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=
+=3D"0" style=3D"width:100%">
+          <tbody>
+            <tr>
+              <td style=3D"direction:ltr;font-size:0px;padding:9px 0px 9px =
+0px;text-align:center;vertical-align:top">
+               =20
+           =20
+      <div class=3D"m_902445541223155851mj-column-per-100" style=3D"font-si=
+ze:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:t=
+op;width:100%">
+       =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"vert=
+ical-align:top" width=3D"100%">
+       =20
+            <tbody><tr>
+              <td align=3D"left" style=3D"font-size:0px;padding:15px 15px 1=
+5px 15px;word-break:break-word">
+               =20
+      <div style=3D"font-family:Ubuntu,Helvetica,Arial,sans-serif;font-size=
+:11px;line-height:1.5;text-align:left;color:#000000">
+        <p>${body}</p>
+      </div>
+   =20
+              </td>
+            </tr>
+         =20
+      </tbody></table>
+   =20
+      </div>
+   =20
+         =20
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       =20
+      </div>
+   =20
+     =20
+     =20
+   =20
+     =20
+      <div style=3D"Margin:0px auto;max-width:600px">
+       =20
+        <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=
+=3D"0" style=3D"width:100%">
+          <tbody>
+            <tr>
+              <td style=3D"direction:ltr;font-size:0px;padding:9px 0px 9px =
+0px;text-align:center;vertical-align:top">
+               =20
+           =20
+      <div class=3D"m_902445541223155851mj-column-per-100" style=3D"font-si=
+ze:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:t=
+op;width:100%">
+       =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"vert=
+ical-align:top" width=3D"100%">
+       =20
+            <tbody><tr>
+              <td style=3D"font-size:0px;padding:10px 25px;padding-top:10px=
+;padding-right:10px;padding-bottom:10px;padding-left:25px;word-break:break-=
+word">
+               =20
+      <p style=3D"border-top:solid 3px #000000;font-size:1;margin:0px auto;=
+width:100%">
+      </p>
+     =20
+     =20
+   =20
+   =20
+              </td>
+            </tr>
+         =20
+            <tr>
+              <td align=3D"center" style=3D"font-size:0px;padding:0px 0px 0=
+px 0px;word-break:break-word">
+               =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"bord=
+er-collapse:collapse;border-spacing:0px">
+        <tbody>
+          <tr>
+            <td style=3D"width:600px">
+             =20
+      <img height=3D"auto" src=3D"https://s3-eu-west-1.amazonaws.com/topoli=
+o/uploads/5faadab2e31cd/1605068400.jpg" style=3D"border:0;display:block;out=
+line:none;text-decoration:none;height:auto;width:100%;font-size:13px" width=
+=3D"600">
+   =20
+            </td>
+          </tr>
+        </tbody>
+      </table>
+   =20
+              </td>
+            </tr>
+         =20
+      </tbody></table>
+   =20
+      </div>
+   =20
+         =20
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       =20
+      </div>
+   =20
+     =20
+     =20
+   =20
+     =20
+      <div style=3D"Margin:0px auto;max-width:600px">
+       =20
+        <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=
+=3D"0" style=3D"width:100%">
+          <tbody>
+            <tr>
+              <td style=3D"direction:ltr;font-size:0px;padding:9px 0px 9px =
+0px;text-align:center;vertical-align:top">
+               =20
+           =20
+      <div class=3D"m_902445541223155851mj-column-per-100" style=3D"font-si=
+ze:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:t=
+op;width:100%">
+       =20
+      <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"vert=
+ical-align:top" width=3D"100%">
+       =20
+            <tbody><tr>
+              <td align=3D"center" style=3D"font-size:0px;padding:10px 10px=
+ 10px 10px;word-break:break-word">
+               =20
+     =20
+    =20
+              <table align=3D"center" border=3D"0" cellpadding=3D"0" cellsp=
+acing=3D"0" style=3D"float:none;display:inline-table">
+               =20
+      <tbody><tr>
+        <td style=3D"padding:4px">
+          <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"=
+background:transparent;border-radius:3px;width:35px">
+            <tbody><tr>
+              <td style=3D"font-size:0;height:35px;vertical-align:middle;wi=
+dth:35px">
+                <a href=3D"https://www.facebook.com/saffroncateringgy" targ=
+et=3D"_blank">
+                    <img alt=3D"facebook" height=3D"35" src=3D"https://s3-e=
+u-west-1.amazonaws.com/ecomail-assets/editor/social-icos/outlined/facebook.=
+png" style=3D"border-radius:3px;display:block" width=3D"35">
+                  </a>
+                </td>
+              </tr>
+          </tbody></table>
+        </td>
+       =20
+      </tr>
+   =20
+              </tbody></table>
+           =20
+              <table align=3D"center" border=3D"0" cellpadding=3D"0" cellsp=
+acing=3D"0" style=3D"float:none;display:inline-table">
+               =20
+      <tbody><tr>
+        <td style=3D"padding:4px">
+          <table border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"=
+background:transparent;border-radius:3px;width:35px">
+            <tbody><tr>
+              <td style=3D"font-size:0;height:35px;vertical-align:middle;wi=
+dth:35px">
+                <a href=3D"https://www.instagram.com/saffroncateringgy" tar=
+get=3D"_blank">
+                    <img alt=3D"instagram" height=3D"35" src=3D"https://s3-=
+eu-west-1.amazonaws.com/ecomail-assets/editor/social-icos/outlined/instagra=
+m.png" style=3D"border-radius:3px;display:block" width=3D"35">
+                  </a>
+                </td>
+              </tr>
+          </tbody></table>
+        </td>
+       =20
+      </tr>
+   =20
+              </tbody></table>
+           =20
+   =20
+   =20
+              </td>
+            </tr>
+         =20
+      </tbody></table>
+   =20
+      </div>
+   =20
+         =20
+              </td>
+            </tr>
+          </tbody>
+        </table>
+       =20
+      </div>
+   =20
+     =20
+     =20
+   =20
+   =20
+      </div>
+   =20
+      </div>
+   =20
+</div></div>
+
+
+`
